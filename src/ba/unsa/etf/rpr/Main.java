0691ec6d7239cf1619f96910d8228d105f61e588
Nejra Hosic <nhosic2@etf.unsa.ba>
2020-10-25 02:39:42 +0100

    Promijenjen tekst koji se ispisuje

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Dobro došli na RPR");
+        System.out.println("Pozdrav svima koliko god vas ima");
     }
 }
